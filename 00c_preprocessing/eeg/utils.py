588b35662f97af13d22d60f448976289b0eee644
gifale95 <alessandro.gifford@gmail.com>
2026-06-01 23:14:12 +0200

    edit

---
+++ 00c_preprocessing/eeg/utils.py
@@ -0,0 +1,183 @@
+def preprocess_eeg(args, session):
+    """Load the raw EEG data and perform filtering, epoching, rejection and
+    interpolation of bad epochs/channels, baseline correction, and frequency
+    downsampling.
+
+    Parameters
+    ----------
+    args : Namespace
+        Input arguments.
+    session : str
+        EEG recording session.
+
+    Returns
+    -------
+    eeg : list of float
+        Preprocessed EEG data.
+    stimulus_id : list of int
+        Stimulus condition IDs.
+    run_number : list of int
+        Run number of each trial of a given session (in the range [1, 16]).
+    trial_number : list of int
+        Number of each trial of a given session (in the range [1, 66]).
+    ch_names : list of str
+        EEG channel names.
+    times : float
+        EEG time points.
+
+    """
+
+    import os
+    import numpy as np
+    from glob import glob
+    import mne
+    import pandas as pd
+    from autoreject import AutoReject
+
+    ### Get the EEG files ###
+    eeg_files = glob(os.path.join(args.project_dir, f'sub-{args.subject:02}',
+        f'ses-{session:02}', 'eeg', '*.vhdr'))
+    eeg_files.sort()
+
+    ### Get the event files ###
+    event_files = glob(os.path.join(args.project_dir, f'sub-{args.subject:02}',
+        f'ses-{session:02}', 'eeg', '*events.tsv'))
+    event_files.sort()
+
+    ### Loop across runs ###    
+    for f in range(len(eeg_files)):
+
+        ### Load the raw EEG data ###
+        # Load the raw EEG data
+        raw = mne.io.read_raw_brainvision(eeg_files[f], preload=True)
+        # Filter the data
+        if args.highpass != None and args.lowpass != None:
+            raw = raw.copy().filter(l_freq=args.highpass, h_freq=args.lowpass)
+
+        ### Epoch the EEG ###
+        # Get the event samples info
+        events, _ = mne.events_from_annotations(raw)
+        # Epoch the EEG trials
+        epochs = mne.Epochs(raw, events, tmin=args.tmin, tmax=args.tmax,
+            baseline=None, preload=True)
+        del raw
+
+        ### Resample ###
+        if args.sfreq < 1000:
+            epochs.resample(args.sfreq)
+
+        ### Reject and interpolate bad epochs and channels ###
+        ar = AutoReject(
+            n_interpolate=[1, 2, 4, 8, 16, 32],
+            random_state=20200220,
+            verbose=True
+        )
+        epochs_clean, reject_log = ar.fit_transform(epochs, return_log=True)
+        bad_epochs = np.where(reject_log.bad_epochs)[0]
+        del epochs
+        # reject_log.plot()
+
+        ### Baseline correction ###
+        if args.baseline_correction == 1:
+            epochs_clean.apply_baseline(baseline=(None, 0))
+
+        ### Get the channel names and time points ###
+        ch_names = epochs_clean.info['ch_names']
+        times = epochs_clean.times
+
+        ### Match the EEG events with the stimulus IDs ###
+        events = epochs_clean.events
+        events_tsv = pd.read_csv(event_files[f], sep="\t")
+        stim_id = np.delete(np.asarray(events_tsv['stim_id']), bad_epochs)
+        stim_id_short = []
+        for s in stim_id:
+            stim_id_short.append(int(str(s)[:2]))
+        if not all(events[:,2] == np.asarray(stim_id_short)):
+            raise Exception('EEG events do not match with stimulus IDs')
+        del events
+
+        ### Store the preprocessed EEG and the trial information ###
+        trial_n = np.delete(np.asarray(events_tsv['trial_num']), bad_epochs)
+        if len(trial_n) != len(stim_id):
+            raise Exception(f'Trial number {len(trial_n)} does not match with stimulus presentation order {len(stim_id)}!')
+        if f == 0:
+            eeg = epochs_clean.get_data().astype(np.float32)
+            stimulus_id = stim_id
+            run_number = np.full_like(stim_id, f+1)
+            trial_number = trial_n
+        else:
+            eeg = np.append(eeg, epochs_clean.get_data().astype(np.float32), 0)
+            stimulus_id = np.append(stimulus_id, stim_id)
+            run_number = np.append(run_number, np.full_like(stim_id, f+1))
+            trial_number = np.append(trial_number, trial_n)
+        del epochs_clean
+
+    ### Output ###
+    return eeg, stimulus_id, run_number, trial_number, ch_names, times
+
+
+def compute_ncsnr(preprocessed_eeg, stimulus_id):
+    """Compute the noise ceiling as in the NSD paper, using the test split
+    data.
+
+    Parameters
+    ----------
+    preprocessed_eeg : list of float
+        Preprocessed EEG data.
+    stimulus_id : list of int
+        Stimulus condition IDs.
+
+    Returns
+    -------
+    ncsnr : float
+        Noise ceiling signal-to-noise ratio.
+    noise_ceiling : float
+        Noise ceiling.
+
+    """
+
+    import numpy as np
+    from sklearn.preprocessing import StandardScaler
+
+    ### Standardize the data at each scan session ###
+    for s in range(len(preprocessed_eeg)):
+        eeg_shape = preprocessed_eeg[s].shape
+        eeg_sess = np.reshape(preprocessed_eeg[s], (eeg_shape[0],-1))
+        scaler = StandardScaler()
+        eeg_sess = scaler.fit_transform(eeg_sess)
+        if s == 0:
+            zscored_data = np.reshape(eeg_sess, eeg_shape)
+            stim_id = stimulus_id[s]
+        else:
+            zscored_data = np.append(zscored_data, np.reshape(eeg_sess, 
+                eeg_shape), 0)
+            stim_id = np.append(stim_id, stimulus_id[s], 0)
+
+    ### Select the test split data ###
+    test_video_cond = np.arange(1001, 1103)
+    test_video_rep = 3 * len(preprocessed_eeg)
+    # Test split data array of shape:
+    # (Video conditions × EEG repetitions × EEG channels × EEG time points)
+    test_data = np.zeros((len(test_video_cond), test_video_rep, eeg_shape[1],
+        eeg_shape[2]))
+    # Index the test split data
+    for v, video in enumerate(test_video_cond):
+        idx_videos = np.where(stim_id == video)[0]
+        if len(idx_videos) != test_video_rep:
+            raise Exception('Wrong test video condition repetition amount!')
+        test_data[v] = zscored_data[idx_videos]
+
+    ### Compute the ncsnr ###
+    tot_var = np.nanvar(np.reshape(test_data,
+        (-1, test_data.shape[2], test_data.shape[3])), axis=0, ddof=1)
+    std_noise = np.sqrt(np.mean(np.nanvar(test_data, axis=1, ddof=1), 0))
+    std_signal = tot_var - (std_noise ** 2)
+    std_signal[std_signal<0] = 0
+    std_signal = np.sqrt(std_signal)
+    ncsnr = std_signal / std_noise
+
+    ### Compute the noise ceiling ###
+    noise_ceiling = 100 * ((ncsnr ** 2) / ((ncsnr ** 2) + (1 / test_video_rep)))
+
+    ### Output ###
+    return ncsnr, noise_ceiling
---
+++ 00c_preprocessing/eyetracking/run/run_preprocess_eyetracking.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --mail-user=giffordale95@zedat.fu-berlin.de
+#SBATCH --job-name=EMD-00c_preprocessing-preprocess_eyetracking
+#SBATCH --mail-type=end
+#SBATCH --mem=1200
+#SBATCH --time=00:15:00
+#SBATCH --qos=extended
+
+# Creating the parameters combinations
+declare -a subject_all
+index=0
+
+for s in `seq 1 6` ; do
+    subject_all[$index]=$s
+    ((index=index+1))
+done
+
+# Extracting the parameters
+echo SLURM_ARRAY_JOB_ID: $SLURM_ARRAY_TASK_ID
+subject=${subject_all[$SLURM_ARRAY_TASK_ID]}
+echo subject: $subject
+
+# Changing to the .py script directory
+cd /home/giffordale95/projects/eeg_moments_dataset/github/EMD/00c_preprocessing/eyetracking
+
+# Activating the Anaconda environment
+source /home/giffordale95/anaconda3/etc/profile.d/conda.sh
+conda activate general
+
+# Running the job
+python preprocess_eyetracking.py --subject $subject
